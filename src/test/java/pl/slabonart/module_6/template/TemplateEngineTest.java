55fb6972226b3096d8357fe447e0bd71eda104ef
Artur_Slabon <artur_slabon@epam.com>
2024-12-16 21:48:51 +0100

    Commit 4
    - values with tag test

--- src/test/java/pl/slabonart/module_6/template/TemplateEngineTest.java
+++ src/test/java/pl/slabonart/module_6/template/TemplateEngineTest.java
@@ -23,7 +23,9 @@ class TemplateEngineTest {
     private static final String TWO_PLACEHOLDERS_TEMPLATE_MESSAGE = "Hello #{name}, #{subject} !!!";
     private static final Map<String, String> TEST_VALUES = Map.of("subject", "subject_value");
     private static final Map<String, String> TEST_VALUES_2 = Map.of("subject", "subject_value", "name", "name_value");
+    private static final Map<String, String> TEST_VALUES_WITH_TAG = Map.of("subject", "#{subject_value}", "name", "#{name_value}");
     private static final String EXPECTED_MESSAGE = "Hello subject_value !!!";
+    private static final String EXPECTED_MESSAGE_WITH_TAG = "Hello #{subject_value} !!!";
     private static final String NO_NAME_VALUE_ERROR_MESSAGE = "Value for placeholder 'name' was not provided";
 
     @Mock
@@ -113,6 +115,32 @@ class TemplateEngineTest {
         String message = fileModeTemplateEngine.generateMessage(template, new Client());
 
         assertEquals(EXPECTED_MESSAGE, message);
+    }
+
+    @Test
+    void givenConsoleTemplateEngine_whenGenerateMessage_andPassValueWithTag_thenReturnProperMessage() throws ValueForPlaceholderNotProvidedException {
+
+        when(valuesLoader.loadValues()).thenReturn(TEST_VALUES_WITH_TAG);
+
+        Template template = new Template();
+        template.setMessage(SIMPLE_TEMPLATE_MESSAGE);
+
+        String message = consoleModeTemplateEngine.generateMessage(template, new Client());
+
+        assertEquals(EXPECTED_MESSAGE_WITH_TAG, message);
+    }
+
+    @Test
+    void givenFileTemplateEngine_whenGenerateMessage_andPassValueWithTag_thenReturnProperMessage() throws ValueForPlaceholderNotProvidedException {
+
+        when(valuesLoader.loadValues()).thenReturn(TEST_VALUES_WITH_TAG);
+
+        Template template = new Template();
+        template.setMessage(SIMPLE_TEMPLATE_MESSAGE);
+
+        String message = fileModeTemplateEngine.generateMessage(template, new Client());
+
+        assertEquals(EXPECTED_MESSAGE_WITH_TAG, message);
     }
 
 
